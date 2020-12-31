ccd75b1a295e43d4a653cd063bbe61c22630f853
priyadarshi0601 <priyadarshi0601@gmail.com>
2020-12-31 23:01:46 +0530

    changes

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask,render_template,jsonify,Response
 import requests
 import base64,cv2
 import cv2
-import cv2
 from PIL import Image
 import numpy as np
 import keras
@@ -16,7 +15,6 @@ import numpy as np
 import argparse 
 import imutils 
 import dlib 
-import cv2
 import pandas as pd
 from imutils.video import WebcamVideoStream
 
@@ -37,9 +35,9 @@ def gen(camera,m,d,p):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 @app.route('/video_feed')
 def video_feed():
-    cnn_model=keras.models.load_model(r'C:\Users\Priyadarshi 2019\Downloads\1dcnnv2.h5')
+    cnn_model=keras.models.load_model('1dcnnv2.h5')
     detector = dlib.get_frontal_face_detector()
-    predictor=dlib.shape_predictor(r"C:\Users\Priyadarshi 2019\Downloads\shape_predictor_68_face_landmarks.dat")
+    predictor=dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
     print('pp')
     return Response(gen(VideoCamera(),cnn_model,detector,predictor), mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__=="__main__":
