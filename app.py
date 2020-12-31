5a59cb234b1aa05e98fb5ee1f7f8fa3d0201a789
priyadarshi0601 <priyadarshi0601@gmail.com>
2020-12-31 18:32:37 +0530

    third

--- app.py
+++ app.py
@@ -23,3 +23,5 @@ def gen(camera):
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__=="__main__":
+    app.run(debug=True)
